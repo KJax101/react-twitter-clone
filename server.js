039e1ebdaa3dc49d842a46e88e3d1f44b8e98345
KJax101 <keng4105@gmail.com>
2018-11-27 19:48:29 -0500

    Added Server/Backend to the App

---
+++ server.js
@@ -0,0 +1,44 @@
+// 2-8 package-json dependencies that the app depends on
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+// Import routes
+const routes = require('./routes');
+// port lcoation pointer
+const PORT = process.env.PORT || 3001;
+
+// Set up Express and defining everything we will be using as 'app.use'
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev')); // for logging
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serve up static assets
+app.use(express.static('client/build'));
+// Add routes, both API and route to client/build
+app.use(routes);
+
+// Set up passport to authenticate
+// const User = require('./models/user');
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mern-auth-demo');
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
